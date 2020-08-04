4e4bcff9765f50aa9666868eed9a5b99d2ad2ba6
chenyazhi <18322329009@163.com>
2020-08-04 09:48:08 +0800

    数据分析

--- src/pages/dataManage/curveAnalysis.vue
+++ src/pages/dataManage/curveAnalysis.vue
@@ -1,3 +1,298 @@
 <template>
-    <div>数据曲线分析</div>
+    <div class="tissue-box" :style="{height: height+'px'}">
+        <div class="tissue-tree">
+            <div class="tissue-left">
+                <div class="search-input">
+                    <input type="text" placeholder="输入组合/测点名称以检索">
+                    <Dropdown style="margin-left: 10px;display:inline-block" trigger="click" :on-click="changeItem(el)">
+                        <a href="javascript:void(0)">
+                            曲线模版
+                            <Icon type="ios-arrow-down"></Icon>
+                        </a>
+                        <DropdownMenu slot="list">
+                            <DropdownItem>选择模板</DropdownItem>
+                            <DropdownItem>保存模板</DropdownItem>
+                        </DropdownMenu>
+                    </Dropdown>
+                </div>
+                
+            </div>
+            <div class="tissue-right">
+                右边
+                
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    name: 'tissueInfor',
+    data () {
+        return {
+           
+            
+        }
+    },
+    mounted() {
+        this.height = document.body.clientHeight-70
+    },
+    methods: {
+        renderContent (h, { root, node, data }) {
+            return h('span', {
+                style: {
+                    display: 'inline-block',
+                    width: '100%'
+                },
+                on: {
+                    //鼠标进入
+                    'mouseenter': () => {
+                        data.is_show = true;
+                    },
+                    //鼠标离开
+                    'mouseleave': () => {
+                        data.is_show = false;
+                    }
+                }
+            }, [
+                h('span', [
+                    h('span', data.title)
+                ]),
+                h('span', {
+                    style: {
+                        display: 'inline-block',
+                        float: 'right'
+                    }
+                }, [
+                    h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            type: 'primary'
+                        }),
+                        style: {
+                            marginRight: '4px',
+                            display: data.is_show ? 'inline-block' : 'none'
+                        },
+                        on: {
+                            // click: () => { this.append(data) }
+                        }
+                    },'编辑'),
+                    h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            type: 'primary'
+                        }),
+                        style: {
+                            display: data.is_show ? 'inline-block' : 'none'
+                        },
+                        on: {
+                            // click: () => { this.remove(root, node, data) }
+                        }
+                    },'删除')
+                ])
+            ]);
+        },
+        renderContentList (h, { root, node, data }) {
+            return h('span', {
+                style: {
+                    display: 'inline-block',
+                    width: '100%'
+                },
+                on: {
+                    //鼠标进入
+                    'mouseenter': () => {
+                        data.is_show = true;
+                    },
+                    //鼠标离开
+                    'mouseleave': () => {
+                        data.is_show = false;
+                    }
+                }
+            }, [
+                h('span', [
+                    h('span', data.title),
+                ]),
+                h('span', {
+                    style: {
+                        display: 'inline-block',
+                        float: 'right'
+                    }
+                }, [
+                    h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            type: 'primary'
+                        }),
+                        style: {
+                            marginRight: '4px',
+                            fontSize:'13px',
+                            display: data.is_show ? 'inline-block' : 'none'
+                        },
+                        on: {
+                            // click: () => { this.append(data) }
+                        }
+                    },'编辑'),
+                    h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            type: 'primary'
+                        }),
+                        style: {
+                            marginRight: '4px',
+                            fontSize:'13px',
+                            display: data.is_show ? 'inline-block' : 'none'
+                        },
+                        on: {
+                            // click: () => { this.remove(root, node, data) }
+                        }
+                    },'删除'),
+                    h('Button', {
+                        props: Object.assign({}, this.buttonProps, {
+                            type: 'primary'
+                        }),
+                        style: {
+                            fontSize:'13px',
+                            display: data.is_show ? 'inline-block' : 'none'
+                        },
+                        on: {
+                            // click: () => { this.remove(root, node, data) }
+                        }
+                    },'新建')
+                ])
+            ]);
+        },
+        append (data) {
+            const children = data.children || [];
+            children.push({
+                title: 'appended node',
+                expand: true
+            });
+            this.$set(data, 'children', children);
+        },
+        remove (root, node, data) {
+            const parentKey = root.find(el => el === node).parent;
+            const parent = root.find(el => el.nodeKey === parentKey).node;
+            const index = parent.children.indexOf(data);
+            parent.children.splice(index, 1);
+        },
+        newFun() {
+            let self = this
+            self.appear = true
+            self.listshow = false
+        },
+        cancel() {
+            let self = this
+            self.appear = false
+            self.appearOther = true
+            
+        },
+        changeItem(e){
+            console.log(e)
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.tissue-box {
+    margin-top: 5px;
+    .tissue-tree {
+        display: flex;
+        min-height: 100%;
+        height: 100%;
+        .search-input{   
+            padding: 2px;
+            border-bottom: 1px solid #f0f0f0;
+            input{
+                width: 195px;
+                border: 1px solid #eaeaea;
+                padding: 0 24px 0 8px;
+                color: #495060;
+                font-size: 13px;
+                 height: 28px;
+                 vertical-align: middle;
+            }
+            a{
+                font-size:12px;
+                height:28px;
+                line-height: 28px;
+                background:#576374;
+                color: #fff;
+                margin-left: 5px;
+                vertical-align: middle;
+                display: inline-block;
+                padding: 0 10px;
+                border-radius: 3px;
+            }
+        }
+        
+        .tissue-left {
+            width: 300px;
+            background: #FFF;
+            height: 100%;
+            .tissue-list {
+                width: 100%;
+                padding: 0 10px;
+                
+            }
+        }
+        .tissue-right {
+            flex: 1;
+            min-height: 100%;
+            height: 100%;
+            width: 100%;
+            background: #FFF;
+            border-left: 5px solid #f5f7f9;
+            border-right: 5px solid #f5f7f9;
+            .tissue-content {
+                padding: 20px 50px;
+                /deep/.ivu-input {
+                    width: 300px;
+                }
+                /deep/.ivu-form .ivu-form-item-label {
+                    padding-right: 0;
+                }
+                .form-li {
+                    border-bottom: 1px dashed #f0f0f0;
+                    font-size: 13px;
+                    h4 {
+                        height: 40px;
+                        margin-top: 20px;
+                    }
+                    /deep/.ivu-form-item {
+                        margin-bottom: 14px;
+                    }
+                    
+                }
+                .form-box {
+                    .form-title {
+                        background-color: #f4f4f4;
+                        height: 30px;
+                        line-height: 30px;
+                        text-indent: 20px;
+                        font-weight: 100;
+                        font-size: 13px;
+                        margin: 10px 0px;
+                    }
+                }
+                .form-other {
+                    .other-li {
+                        display: flex;
+                        font-size: 13px;
+                        line-height: 30px;
+                        span:nth-of-type(1) {
+                            width: 70px;
+                            text-align: right;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    /deep/.ivu-row {
+        margin: 0!important;
+        /deep/.ivu-col{
+            background: #fff;
+            padding: 0!important;
+        }
+        /deep/.ivu-col-span-xl-6 {
+            width: 24%;
+            margin-right: 1%;
+        }
+    }
+}
+</style>
--- src/pages/dataManage/labourData.vue
+++ src/pages/dataManage/labourData.vue
@@ -1,3 +1,248 @@
 <template>
-    <div>人工数据采集</div>
+    <div class="index-box" :style="{height: height+'px'}">
+        <div class="index-search" :class="{searchTrans:searchShow, searchPack:!searchShow}">
+            <div class="search-main">
+                <div class="form-item">
+                    <label>关键字：</label>
+                    <Input v-model="keyword" placeholder="录入名称" style="width: 300px" />
+                </div>
+                <div class="form-item">
+                    <label>区域位置：</label> 
+                    <Select v-model="model1" style="width:200px">
+                        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                    </Select>
+                </div>
+                <div class="form-search-btn">
+                    <a href="javascript:;" @click="higherSearch()">
+                        <Icon type="ios-arrow-down" v-if="searchShow" />
+                        <Icon type="ios-arrow-up" v-else />
+                        高级搜索
+                    </a>
+                    <button type="button">搜索</button>
+                    <button type="button" class="reset">重置</button>
+                </div>
+            </div>
+            <div class="c-adv-search">
+                <div class="c-adv-search-row">
+                    <div class="form-item">
+                        <label>业务时间：</label>
+                        <div class="cmp-tab">
+                           <DatePicker type="date" :options="startTime" placeholder="开始日期" style="width: 190px"></DatePicker> -
+                           <DatePicker type="date" :options="endTime" placeholder="结束日期" style="width: 190px"></DatePicker>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="index-content">
+            <div class="c-table-top-btns">
+                <button type="button" @click="addNew()">新增</button>
+                 <button type="button" @click="deleteNew()" style="margin-left:10px">删除</button>
+            </div>
+            <div class="table-wrapper" :style="{height: (height-45)+'px'}">
+                <Table stripe :columns="tableList" :data="tableData">
+                    <template slot-scope="{ row }" slot="name">
+                        <strong>{{ row.name }}</strong>
+                    </template>
+                    <template slot-scope="{ row, index }" slot="action">
+                        <Button class="action" size="small" style="margin-right: 5px;">配置</Button>
+                        <Button class="action" size="small">测试</Button>
+                    </template>
+                </Table>
+                 <Page :total="100" show-elevator class="page" />
+            </div>
+        </div>
+    </div>
 </template>
+<script>
+export default {
+    name: 'indexManage',
+    data() {
+        return {
+            height: '',
+            keyword: '',
+            cityList: [
+                {
+                    value: 'New York',
+                    label: 'New York'
+                }
+            ],
+            model1: '',
+            searchShow: false,
+            typeCheckedAll: false,
+            typeBox: [],
+            tableList: [
+                {
+                    type: 'selection',
+                    width: 50,
+                    align: 'center'
+                },
+                {
+                    title: '区域位置',
+                    key: 'location'
+                },
+                {
+                    title: '录入名称',
+                    key: 'name'
+                },
+                {
+                    title: '录入周期',
+                    key: 'duration'
+                },
+                {
+                    title: '最近业务时间',
+                    key: 'businessTime'
+                },
+                {
+                    title: '最近录入时间',
+                    key: 'enterTime'
+                },
+                {
+                    title: '操作',
+                    slot: 'action',
+                    width: 150,
+                    align: 'center'
+                }
+            ]
+        }
+    },
+    mounted() {
+        this.height = document.body.clientHeight-130
+    },
+    methods: {
+        higherSearch() {
+            this.searchShow = !this.searchShow
+        },
+        typeCheckAll() {
+            this.typeBox = []
+            this.typeCheckedAll = true
+        },
+        typeCheck(i) {
+            this.typeCheckedAll = false
+            if(this.typeBox.includes(i)) {
+                this.typeBox = this.typeBox.filter((ele) => {
+                    return ele != i
+                });
+            } else {
+                this.typeBox.push(i);
+            }
+        },
+        addNew() {
+            this.$router.push({path:'/systemManage/index/indexAdd'})
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.index-box {
+    margin: 5px;
+    background: #fff;
+    .index-search {
+        padding: 5px;
+        height: 43px;
+        .search-main {
+            height: 33px;
+            .form-item {
+                display: inline-block;
+                min-height: 33px;
+                label {
+                    display: inline-block;
+                    width: 100px;
+                    line-height: 35px;
+                    text-align: right;
+                    color: #576374;
+                }
+            }
+            .form-search-btn {
+                float: right;
+                margin-top: 4px;
+                a {
+                    margin: 0 10px 0 0;
+                    color: #576374;
+                    font-size: 12px;
+                }
+                button{
+                    background: #4b7efe;
+                    font-size: 12px;
+                    padding: 4px 12px;
+                    color: #fff;
+                    border: 0;
+                    border-radius: 3px;
+                    margin: 0 5px;
+                }
+                .reset{
+                    background: #495566;
+                }
+            }
+        }
+        .c-adv-search {
+            margin-top: 10px;
+            padding-top: 5px;
+            border-top: 1px solid #ececec;
+            .c-adv-search-row {
+                margin: 5px 0;
+                .form-item {
+                    display: inline-block;
+                    height: 33px;
+                    label {
+                        display: inline-block;
+                        width: 100px;
+                        line-height: 35px;
+                        text-align: right;
+                        color: #576374;
+                    }
+                }
+                .cmp-tab {
+                    display: inline-block;
+                    a {
+                        margin-right: 20px;
+                        color: #576374;
+                    }
+                    .checked {
+                        color: #4B7EFE;
+                    }
+                }
+            }
+        }
+    }
+    .searchTrans {
+        height: 100px;
+        overflow: hidden;
+        transition: 0.5s height;
+    }
+    .searchPack {
+        height: 43px;
+        overflow: hidden;
+        transition: 0.5s height;
+    }
+    .index-content {
+        border-top: 5px solid #f0f0f0;
+        padding: 10px;
+        .c-table-top-btns {
+            height: 36px;
+            border-bottom: 1px solid #EEE;
+            button{
+                min-width: 50px;
+                background: #576374;
+                font-size: 12px;
+                padding: 4px 12px;
+                color: #fff;
+                border: 0;
+                border-radius: 3px;
+            }
+        }
+        .table-wrapper{
+            .action{
+                color:rgb(75, 126, 254);
+                border:0;
+                font-size:12px;
+                background:transparent
+            }
+            .page{
+                text-align: right;
+                margin-top: 10px;
+            }
+        }
+    }
+}
+</style>
