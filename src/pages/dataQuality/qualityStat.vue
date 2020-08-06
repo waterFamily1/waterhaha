9c94c9b8726207874a118e2992b1fe9e60baab0f
ellaWLY <13820451821@163.com>
2020-08-06 14:45:44 +0800

    数据质量

--- src/pages/dataQuality/qualityStat.vue
+++ src/pages/dataQuality/qualityStat.vue
@@ -25,10 +25,64 @@
             </div>
         </div>
         <div class="quality-border">
-            1
+            <div class="box">
+                <div class="title">总体评价</div>
+                <div class="content">
+            
+                </div>
+            </div>
+            <ul class="evaluate-list">
+                <li>
+                    <div class="content">
+                        <span class="evaluate-result"></span>
+                        <div class="right-content">
+                            <span class="title">准确性</span>
+                            <div title="%" class="evaluate-index ell">%</div>
+                        </div>
+                    </div>
+                    <p>准确数据量/实际总数据量</p>
+                </li>
+                <li>
+                    <div class="content">
+                        <span class="evaluate-result"></span>
+                        <div class="right-content">
+                            <span class="title">完整性</span>
+                            <div title="%" class="evaluate-index ell">%</div>
+                        </div>
+                    </div>
+                    <p>实际数据量/理论数据量</p>
+                </li>
+                <li>
+                    <div class="content">
+                        <span class="evaluate-result"></span>
+                        <div class="right-content">
+                            <span class="title">及时性</span>
+                            <div title="%" class="evaluate-index ell">%</div>
+                        </div>
+                    </div>
+                    <p>平均采集延时</p>
+                </li>
+                <li>
+                    <div class="content">
+                        <span class="evaluate-result"></span>
+                        <div class="right-content">
+                            <span class="title">一致性</span>
+                            <div title="%" class="evaluate-index ell">%</div>
+                        </div>
+                    </div>
+                    <p>一致数据量/实际总数据量</p>
+                </li>
+            </ul>
+            <div class="box">
+                <div class="title">样本占比</div>
+            </div>
         </div>
         <div class="quality-border">
-            2
+            <div class="c-table-top-btns">
+                <Button>重新计算</Button>
+                <Button>重算日志</Button>
+            </div>
+            <Table ref="selection" :columns="columns" :data="data"></Table>
         </div>
     </div>
 </template>
@@ -80,7 +134,55 @@ export default {
                     value: '3',
                     label: '年'
                 }
-            ]
+            ],
+            columns: [
+                {
+                    type: 'selection',
+                    width: 60,
+                    align: 'center'
+                },
+                {
+                    title: '序号',
+                    key: 'number'
+                },
+                {
+                    title: '测点名称',
+                    key: 'stationName'
+                },
+                {
+                    title: '区域位置',
+                    key: 'location'
+                },
+                {
+                    title: '数据量',
+                    key: 'dataSize'
+                },
+                {
+                    title: '准确性',
+                    key: 'accuracy'
+                },
+                {
+                    title: '完整性',
+                    key: 'integrity'
+                },
+                {
+                    title: '及时性',
+                    key: 'timeliness'
+                },
+                {
+                    title: '一致性',
+                    key: 'consistency'
+                },
+                {
+                    title: '所属设备',
+                    key: 'facility'
+                },
+                {
+                    title: '统计时间',
+                    key: 'statisticsTime'
+                }
+            ],
+            data: []
         }
     },
     mounted() {
@@ -143,6 +245,113 @@ export default {
     .quality-border {
         border-top: 5px solid #f0f0f0;
         padding: 10px;
+        .box {
+            width: 20%;
+            color: #576374;
+            background-color: #f1f3f8;
+            display: inline-block;
+            vertical-align: top;
+            position: relative;
+            .title {
+                height: 80px;
+                width: 40px;
+                word-break: break-all;
+                line-height: 20px;
+                padding-left: 15px;
+                padding-right: 10px;
+                margin-top: 8px;
+                margin-bottom: 8px;
+                font-size: 12px;
+                color: #333E4E;
+                border-right: 1px solid #e4e9f1;
+            }
+            .content {
+                position: absolute;
+                height: 96px;
+                display: inline-block;
+                left: 50px;
+                right: 0;
+                top: 0;
+            }
+        }
+        .evaluate-list {
+            display: inline-block;
+            width: 58%;
+            margin: 0 0.5%;
+            background-color: #f1f3f8;
+            overflow: hidden;
+            height: 96px;
+            vertical-align: top;
+            li {
+                list-style: none;
+                float: left;
+                width: 25%;
+                position: relative;
+                text-align: center;
+                .content {
+                    min-width: 120px;
+                    height: 57px;
+                    margin: 8px auto 0;
+                    .evaluate-result {
+                        display: inline-block;
+                        width: 36px;
+                        height: 36px;
+                        border-radius: 18px;
+                        text-align: center;
+                        line-height: 36px;
+                        font-size: 16px;
+                        margin: 10px 10px 10px 0px;
+                    }
+                    .right-content {
+                        display: inline-block;
+                        vertical-align: top;
+                        margin-top: 10px;
+                        text-align: left;
+                        .title {
+                            color: #333E4E;
+                        }
+                        .evaluate-index {
+                            font-size: 16px;
+                            font-weight: bold;
+                            height: 22px;
+                            line-height: 22px;
+                            width: 95px;
+                            color: #333E4E;
+                        }
+                    }
+                }
+                p {
+                    font-size: 10px;
+                    color: #686F7B;
+                }
+            }
+            li:before {
+                content: "";
+                position: absolute;
+                left: 0;
+                top: 18px;
+                height: 60px;
+                width: 1px;
+                background-color: #e4e9f1;
+            }
+        }
+        .c-table-top-btns {
+            height: 36px;
+            border-bottom: 1px solid #EEE;
+            /deep/.ivu-btn {
+                background-color: #576374;
+                height: auto;
+                color: #fff;
+                padding: 4px 12px;
+                font-size: 13px;
+                margin-right: 5px;
+            }
+        }
+    }
+    .ell {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
     }
 }
 </style>
